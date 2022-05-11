bbd6bd851c4e22bc91f25031de86252dfc6225c0
BarayAlberto <bto450xd@gmail.com>
2022-05-11 11:11:06 -0600

    Creacion del nucleo del servidor

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var app = express();
+//Require los paquetes y definimos el puerto
+const express = require('express');
+const port = 3002;
 
-app.get('/hola', function(req, res){
-    res.send('Hola Mundo');
-});
+//Para permitir el manejo de POST y PUT
+const bodyParser = require('boy-parser');
+const routes = require('./routes/routes');
+const app = express();
 
-app.get('/adios', function(req, res){
-    res.send('Adios Mundo');
-});
+//Usarn Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true,
+}));
 
-app.listen(3000, function(){
-    console.log('Aplicacion de ejemplo, escuchando el puerto 3000!')
+routes(app);
+
+//Iniciar el servidor
+const server = app.listen(port, (error) =>{
+    if(error) return console.log(`Error: ${error}`);
+
+    console.log(`El servidor escucha en el puerto ${server.address().port}`)
 });
